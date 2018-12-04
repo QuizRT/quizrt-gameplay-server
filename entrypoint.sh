47b8d7f9fa3910622814bdc62e6afb28ab710e4a
Kuldeep Sharma <kul123sharma@gmail.com>
2018-12-04 15:41:35 +0530

    Removed-DB-Service-From-Docker-Compose-And-Removed-EFUpdate-Command-From-EntryPoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 set -e
 run_cmd="dotnet run --server.urls http://*:82"
 
-until dotnet ef database update; do
->&2 echo "SQL Server is starting up"
-sleep 1
-done
+# until dotnet ef database update; do
+# >&2 echo "SQL Server is starting up"
+# sleep 1
+# done
 
->&2 echo "SQL Server is up - executing command"
+>&2 echo "Executing command"
 exec $run_cmd
